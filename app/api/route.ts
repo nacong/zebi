efaaeb64ec39a1c9a7ec1a889ade3e62e1e6b294
nacong <baskinnabeens@gmail.com>
2026-01-28 09:51:10 +0900

    FIX: 한글 깨짐 오류로, 연결설정에 UTF8 추가

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,7 +3,13 @@ import postgres from "postgres";
 import { College } from "../definitions/definition";
 import { NextResponse } from "next/server";
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { 
+  ssl: 'require',
+  connection: {
+    application_name: 'zebi-app',
+    client_encoding: 'UTF8'
+  }
+});
 
 export async function GET() {
   const colleges = await sql<College[]>`SELECT id, name FROM users;`;
