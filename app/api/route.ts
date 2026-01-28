cc1c1916152d5095fc922068fa0178f6001bf45c
nacong <baskinnabeens@gmail.com>
2026-01-28 10:17:42 +0900

    FIX: 한글 깨짐 오류 해결방법 변경

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,15 +3,9 @@ import postgres from "postgres";
 import { College } from "../definitions/definition";
 import { NextResponse } from "next/server";
 
-const sql = postgres(process.env.POSTGRES_URL!, { 
-  ssl: 'require',
-  connection: {
-    application_name: 'zebi-app',
-    client_encoding: 'UTF8'
-  }
-});
+const sql = postgres(process.env.POSTGRES_URL!);
 
 export async function GET() {
   const colleges = await sql<College[]>`SELECT id, name FROM users;`;
-  return NextResponse.json(colleges);
+  return NextResponse.json(colleges, { headers: { 'Content-Type': 'application/json; charset=utf-8' }});
 }
